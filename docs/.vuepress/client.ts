e0009d8c25d374c4eb35e5946dfe43a76a22e0f1
coderclc <804748585@qq.com>
2022-08-05 20:20:00 +0800

    feat: update

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,9 +1,5 @@
 import { defineClientConfig } from '@vuepress/client'
 
-import VConsole from 'vconsole'
-
-const vConsole = new VConsole()
-
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {},
